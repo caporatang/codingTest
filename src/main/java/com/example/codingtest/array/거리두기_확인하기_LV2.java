8c8e01d4844738d80086a8342b6bb0433752ceb7
caporatang <caporatang@naver.com>
2023-12-03 22:39:16 +0900

    LV2 행렬의 곱셈, 거리두기 확인하기

---
+++ src/main/java/com/example/codingtest/array/거리두기_확인하기_LV2.java
@@ -0,0 +1,77 @@
+package com.example.codingtest.array;
+
+public class 거리두기_확인하기_LV2 {
+    // 개발자를 희망하는 죠르디는 카카오에 면접을 보러왔다.
+    // 코로나 바이러스 감염 예방을 위해 응시자들은 거리를 둬서 대기를 해야하는데
+    // 개발 직군 면접인 만큼 아래와 같은 규칙으로 대기실에 거리를 두고 앉도록 안내하고 있다.
+
+    // 1. 대기실은 5개이며, 각 대기실은 5x5 크기이다.
+    // 2. 거리두기를 위하여 응시자들끼리 맨해튼 거리가 2 이하로 앉지 말아 주세요
+    // 3. 단 응시자가 앉아 있는 자리 사이가 파티션으로 막혀 있을 경우에는 허용한다.
+    // ㅎ므.........흐...................................이ㅏ해가 잘 안되는데
+
+
+    private static final int dx[] = {0, -1, 1, 0};
+    private static final int dy[] = {-1, 0, 0, 1};
+
+    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
+        for (int d = 0; d < 4; d++) {
+            if (d == exclude) continue;
+
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length)
+                continue;
+            if (room[ny][nx] == 'P') return true;
+        }
+        return false;
+    }
+
+    private boolean isDistanced(char[][] room, int x, int y) {
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length)
+                continue;
+
+            switch (room[ny][nx]) {
+                case 'P': return false;
+                case 'O':
+                    if (isNextToVolunteer(room, nx, ny, 3- d)) return false;
+                    break;
+            }
+        }
+        return true;
+    }
+
+    private boolean isDistanced(char[][] room) {
+        for (int y = 0; y < room.length; y++) {
+            for (int x = 0; x < room[y].length; x++) {
+                if (room[y][x] != 'P') continue;
+                if (!isDistanced(room, x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+        for (int i = 0; i < answer.length; i++) {
+            String[] place = places[i];
+            char[][] room = new char[place.length][];
+            for (int j = 0; j < room.length; j++) {
+                room[j] = place[j].toCharArray();
+            }
+            if (isDistanced(room)) {
+                answer[i] = 1;
+            } else {
+                answer[i] = 0;
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
---
+++ src/main/java/com/example/codingtest/array/행렬의_곱셈_LV2.java
@@ -0,0 +1,21 @@
+package com.example.codingtest.array;
+
+public class 행렬의_곱셈_LV2 {
+    // 2차원 행렬 arr1 과 arr2를 입력받아, arr1에 arr2를 곱한 결과를 반환하는 함수를 리턴
+    // 제한 조건
+    // 1. 행렬 arr1, arr2의 행과 열의 길이는 2이상 100 이하입니다
+    // 2. 행렬 arr1, arr2의 원소는 -10 이상 20 이하인 자연수입니다.
+    // 3. 곱할 수 있는 배열만 주어집니다
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] arr = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = 0;
+                for (int k = 0; k < arr1[i].length; k++) {
+                    arr[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return arr;
+    }
+}
